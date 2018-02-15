56de315c387ad88fdaf8c0c3efe12d473b5c0c59
Miguel Ribeiro <miguel.rib.20@gmail.com>
2018-02-15 17:16:51 +0000

    Get typed field value on submit

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class Form extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(this.refs.userName.value);
+    }
+
     render() {
         return (
             <div className="formWrapper">
-                <form action="" className="formContainer">
+                <form action="" className="formContainer" onSubmit={this.handleSubmit}>
                     <div className="formField">
                         <label htmlFor="userName" className="labelInlineField">User:</label>
-                        <input type="text" name="userName" id="userName"/>
+                        <input type="text" name="userName" id="userName" ref="userName"/>
                     </div>
                     <div className="formField">
                         <input type="submit" valid="Search"/>
